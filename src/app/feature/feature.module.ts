dde3ef9dfe2c6982fb61883e6fd59a7985672fb0
Arpit <arpit_jain@live.com>
2022-07-28 15:17:07 +0530

    adds reactive forms form-control

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { FeatureRoutingModule } from './feature-routing.module';
 import { FeatureComponent as FeatureOneCmp } from './feature.component';
@@ -7,7 +8,7 @@ import { ReactiveFormComponent } from '../reactive-form/reactive-form.component'
 
 @NgModule({
   declarations: [FeatureOneCmp, ReactiveFormComponent],
-  imports: [CommonModule, FeatureRoutingModule],
+  imports: [CommonModule, FeatureRoutingModule, FormsModule, ReactiveFormsModule],
   exports: [FeatureOneCmp, ReactiveFormComponent],
 })
 export class FeatureModule {}
--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.css']
+  styleUrls: ['./reactive-form.component.css'],
 })
 export class ReactiveFormComponent implements OnInit {
+  blogTitle: FormControl;
+  blogDescription: FormControl;
 
-  constructor() { }
+  constructor() {
+    this.blogTitle = new FormControl("", [Validators.required, Validators.maxLength(30)]);
+    this.blogDescription = new FormControl("", [Validators.required, Validators.minLength(50), Validators.maxLength(500)]);
+    console.log(this.blogTitle)
 
-  ngOnInit(): void {
+    this.blogTitle.valueChanges.subscribe(value => {
+      console.log(value, this.blogTitle)
+    })
   }
 
+  ngOnInit(): void {}
+
+  handleFormSubmit(event: any) {
+    console.log(event);
+    event.preventDefault();
+    // alert('Form submitted');
+    console.log('form values', this.blogTitle.value, this.blogDescription.value);
+  }
 }
