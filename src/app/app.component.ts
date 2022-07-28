f6643316a5e9832ce9952bf5d03177ecdae261d4
Arpit <arpit_jain@live.com>
2022-07-28 11:50:34 +0530

    template drive forms way ahead

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,43 @@
 import { Component } from '@angular/core';
 
+class CUser implements IUser {
+  name: string;
+  email: string;
+  password: string;
+
+  constructor(name: string = '', email: string = '', passsword: string = '') {
+    this.name = name;
+    this.email = email;
+    this.password = passsword;
+  }
+}
+
+interface IUser {
+  name: string;
+  email: string;
+  password: string;
+}
+type User = {
+    name: string,
+    email: string,
+    password: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = "Create a Blog";
+  title = 'Create a Blog';
   blogTitle: string = 'Init value';
   blogDescription: string = '';
+  user: CUser = new CUser('John Doe', 'johndoe@mailinator.com', '123456');
 
-  handleFormSubmit(str: string) {
+  handleFormSubmit(event: any) {
+    console.log(event);
+    event.preventDefault();
     // alert('Form submitted');
-    console.log('form values', str, this.blogTitle, this.blogDescription);
+    console.log('form values', this.blogTitle, this.blogDescription, this.user);
   }
 }
