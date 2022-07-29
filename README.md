d67ee280eaa7f0036273c83f8fe4cfeca60cca53
Arpit <arpit_jain@live.com>
2022-07-29 15:25:37 +0530

    routing, nested routing, router-outlet

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - Reactive Forms
 
 ### Routing in angular app steps:
-1. Adding Routing related modules
+1. Adding Routing related modules `RouterModule` imported from `@angular/router`
 2. Setup paths and their associated components
 3. Define the routes for our application
 4. Define outlet
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
-import { FeatureModule } from './feature/feature.module';
 
 // const routes: Routes = [
 //   { path: 'watch/first', component: AppComponent },
